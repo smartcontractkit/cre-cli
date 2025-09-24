2f9efe8b1e5ba6199e6d08d0629211b471f24665
Timothy Funnell <137069066+timothyF95@users.noreply.github.com>
2025-09-25 00:50:04 +0100

    Chore: Removed dead code (#28)
    Removed dead code

--- internal/settings/workflow_storage_settings.go
+++ internal/settings/workflow_storage_settings.go
@@ -2,41 +2,14 @@ package settings
 
 import (
 	"fmt"
-	"os"
-	"strings"
 	"time"
 
 	"github.com/rs/zerolog"
 	"github.com/spf13/viper"
-
-	"github.com/smartcontractkit/cre-cli/cmd/gist"
 )
 
 type WorkflowStorageSettings struct {
-	Gist       GistStorageSettings  `mapstructure:"gist"`
-	Minio      MinioStorageSettings `mapstructure:"minio"`
-	S3         S3StorageSettings    `mapstructure:"s3"`
-	CREStorage CREStorageSettings   `mapstructure:"cre_storage"`
-}
-
-type GistStorageSettings struct {
-	GithubToken gist.GitHubAPIToken `mapstructure:"github_token"`
-}
-
-type MinioStorageSettings struct {
-	Endpoint        string `mapstructure:"endpoint"`
-	AccessKeyID     string `mapstructure:"access_key_id"`
-	SecretAccessKey string `mapstructure:"secret_access_key"`
-	SessionToken    string `mapstructure:"session_token"`
-	UseSSL          bool   `mapstructure:"use_ssl"`
-	Region          string `mapstructure:"region"`
-}
-
-type S3StorageSettings struct {
-	AccessKeyID     string `mapstructure:"access_key_id"`
-	SecretAccessKey string `mapstructure:"secret_access_key"`
-	SessionToken    string `mapstructure:"session_token"`
-	Region          string `mapstructure:"region"`
+	CREStorage CREStorageSettings `mapstructure:"cre_storage"`
 }
 
 type CREStorageSettings struct {
@@ -57,29 +30,5 @@ func LoadWorkflowStorageSettings(logger *zerolog.Logger, v *viper.Viper) Workflo
 		return WorkflowStorageSettings{}
 	}
 
-	// Resolve gist environment variable placeholders
-	settings.Gist.GithubToken = gist.GitHubAPIToken(resolveEnvVars(string(settings.Gist.GithubToken)))
-
-	// Resolve minio environment variable placeholders
-	settings.Minio.AccessKeyID = resolveEnvVars(settings.Minio.AccessKeyID)
-	settings.Minio.SecretAccessKey = resolveEnvVars(settings.Minio.SecretAccessKey)
-	settings.Minio.SessionToken = resolveEnvVars(settings.Minio.SessionToken)
-	settings.Minio.Endpoint = resolveEnvVars(settings.Minio.Endpoint)
-	settings.Minio.Region = resolveEnvVars(settings.Minio.Region)
-
-	// Resolve s3 environment variable placeholders
-	settings.S3.AccessKeyID = resolveEnvVars(settings.S3.AccessKeyID)
-	settings.S3.SecretAccessKey = resolveEnvVars(settings.S3.SecretAccessKey)
-	settings.S3.SessionToken = resolveEnvVars(settings.S3.SessionToken)
-	settings.S3.Region = resolveEnvVars(settings.S3.Region)
-
 	return settings
-}
-
-func resolveEnvVars(value string) string {
-	if strings.HasPrefix(value, "${") && strings.HasSuffix(value, "}") {
-		envVar := strings.Trim(value, "${}")
-		return os.Getenv(envVar)
-	}
-	return value
 }
