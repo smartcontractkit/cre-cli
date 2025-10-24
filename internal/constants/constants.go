8ca5b676955ff62ee889c5ba758d8439cdcbd6f8
Timothy Funnell <137069066+timothyF95@users.noreply.github.com>
2025-10-24 15:20:12 +0100

    improve formatting of cre help (#119)

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -32,9 +32,9 @@ const (
 	DefaultBaseSepoliaChainName = "ethereum-testnet-sepolia-base-1" // Base Sepolia
 	DefaultEthMainnetChainName  = "ethereum-mainnet"                // Eth Mainnet
 
-	DefaultEthSepoliaRpcUrl  = "https://sepolia.infura.io/v3/YOUR_API_KEY" // ETH Sepolia
-	DefaultBaseSepoliaRpcUrl = "<select your own rpc url>"                 // Base Sepolia
-	DefaultEthMainnetRpcUrl  = "<select your own rpc url>"                 // ETH Mainnet
+	DefaultEthSepoliaRpcUrl  = "https://sepolia.infura.io/v3/<YOUR-PROJECT-ID>" // ETH Sepolia
+	DefaultBaseSepoliaRpcUrl = "<select your own rpc url>"                      // Base Sepolia
+	DefaultEthMainnetRpcUrl  = "https://mainnet.infura.io/v3/<YOUR-PROJECT-ID>" // ETH Mainnet
 
 	DefaultStagingDonFamily           = "zone-a" // Keystone team has to define this
 	DefaultProductionTestnetDonFamily = "zone-a" // Keystone team has to define this
