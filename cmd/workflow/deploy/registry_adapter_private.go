b44279686ae35d528d3231d2fd9d0829395c9f54
Jakub Nowak <jakub.nowak@smartcontract.com>
2026-04-13 13:08:03 +0200

    merge input to usage

--- cmd/workflow/deploy/registry_adapter_private.go
+++ cmd/workflow/deploy/registry_adapter_private.go
@@ -64,3 +64,24 @@ func (a *privateRegistryAdapter) Upsert() error {
 
 	return nil
 }
+
+func (h *handler) buildPrivateRegistryInput() privateregistryclient.WorkflowInRegistryInput {
+	input := privateregistryclient.WorkflowInRegistryInput{
+		WorkflowID:   h.workflowArtifact.WorkflowID,
+		Status:       privateregistryclient.WorkflowStatusActive,
+		WorkflowName: h.inputs.WorkflowName,
+		BinaryURL:    h.inputs.BinaryURL,
+		DonFamily:    h.inputs.DonFamily,
+	}
+
+	if configURL := h.inputs.ResolveConfigURL(""); configURL != "" {
+		input.ConfigURL = &configURL
+	}
+
+	if h.inputs.WorkflowTag != "" {
+		tag := h.inputs.WorkflowTag
+		input.Tag = &tag
+	}
+
+	return input
+}
