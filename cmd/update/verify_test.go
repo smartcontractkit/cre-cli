dd50fd66a92ab4f4a7719ca622a789fee809e27b
anirudhwarrier <12178754+anirudhwarrier@users.noreply.github.com>
2026-07-03 13:27:27 +0400

    update tests

--- cmd/update/verify_test.go
+++ cmd/update/verify_test.go
@@ -1,28 +1,13 @@
 package update
 
 import (
-	"os"
-	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
-
-	"github.com/smartcontractkit/cre-cli/install"
 )
-
-func TestReleasePublicKeyMatchesInstall(t *testing.T) {
-	embedded := install.ReleasePublicKey
-	require.NotEmpty(t, embedded)
-
-	canonical, err := os.ReadFile(filepath.Join("public_key.asc"))
-	require.NoError(t, err)
-
-	require.Equal(t, canonical, embedded, "embedded public_key.asc must match install/public_key.asc (symlink target)")
-}
 
 func TestGetSigAssetName(t *testing.T) {
 	require.Equal(t, "cre_linux_amd64.sig", getSigAssetName("linux", "amd64"))
-	require.Equal(t, "cre_darwin_arm64.sig", getSigAssetName("darwin", "arm64"))
 }
 
 func TestGetAssetName(t *testing.T) {
