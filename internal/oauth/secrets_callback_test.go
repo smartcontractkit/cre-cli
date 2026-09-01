3acd51bde31bbca20628dedbe73a20232148d36d
timothyF95 <tim.funnell@smartcontract.com>
2026-09-01 23:15:15 +0100

    chore: adjust state parameter

--- internal/oauth/secrets_callback_test.go
+++ internal/oauth/secrets_callback_test.go
@@ -52,15 +52,29 @@ func TestSecretsCallbackHandler_oauthError(t *testing.T) {
 	assert.Len(t, codeCh, 0)
 }
 
-func TestSecretsCallbackHandler_noStateRequired(t *testing.T) {
+func TestSecretsCallbackHandler_missingState(t *testing.T) {
 	log := zerolog.Nop()
 	codeCh := make(chan string, 1)
-	h := SecretsCallbackHandler(codeCh, "", &log)
+	h := SecretsCallbackHandler(codeCh, "want-state", &log)
 
 	req := httptest.NewRequest(http.MethodGet, "/callback?code=only-code", nil)
 	rr := httptest.NewRecorder()
 	h(rr, req)
 
-	assert.Equal(t, http.StatusOK, rr.Code)
-	assert.Equal(t, "only-code", <-codeCh)
+	assert.Equal(t, http.StatusBadRequest, rr.Code)
+	assert.Len(t, codeCh, 0)
+}
+
+// A caller that fails to supply an expected state must not accidentally disable validation.
+func TestSecretsCallbackHandler_emptyExpectedState(t *testing.T) {
+	log := zerolog.Nop()
+	codeCh := make(chan string, 1)
+	h := SecretsCallbackHandler(codeCh, "", &log)
+
+	req := httptest.NewRequest(http.MethodGet, "/callback?code=c&state=anything", nil)
+	rr := httptest.NewRecorder()
+	h(rr, req)
+
+	assert.Equal(t, http.StatusBadRequest, rr.Code)
+	assert.Len(t, codeCh, 0)
 }
